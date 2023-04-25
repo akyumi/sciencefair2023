4c670aa2ac9ef1fb5b0f19ada0d419ea3513d90d
ernfrncz <126535224+ernfrncz@users.noreply.github.com>
2023-04-25 11:21:34 +0800

    Add files via upload

---
+++ Lithium-1/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("mySidebar").style.width = "100px";
+    document.getElementById("navbar").style.marginRight = "100px";
+}
+  
+function closeNav(){
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("navbar").style.marginRight = "0";
+
+}
