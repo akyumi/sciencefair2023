4b66713c26f7fb5e4015a9b27a4c494e4394a519
Kevin Lee Cardoza <49277166+Kiboot@users.noreply.github.com>
2023-04-26 18:32:31 +0800

    navbar 404 fix attempt 1
    changing redirection of navbar.html

--- js/pageloader.js
+++ js/pageloader.js
@@ -1,5 +1,5 @@
 async function addNavbar() {
-    const resp = await fetch("navbar.html");
+    const resp = await fetch("../navbar.html");
     const html = await resp.text();
     document.body.insertAdjacentHTML("afterbegin", html);
 }
