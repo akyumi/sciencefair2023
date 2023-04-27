dd7ec2560d331a3de77f42477144a01eef5216c9
Kevin Lee Cardoza <49277166+Kiboot@users.noreply.github.com>
2023-04-27 11:56:22 +0800

    replace bs css and js source

--- js/pageloader.js
+++ js/pageloader.js
@@ -1,5 +1,5 @@
 async function addNavbar() {
-    const resp = await fetch("../navbar.html");
+    const resp = await fetch("https://smc-26.github.io/navbar.html");
     const html = await resp.text();
     document.body.insertAdjacentHTML("afterbegin", html);
 }
