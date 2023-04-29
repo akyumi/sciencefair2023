1a6a8a2e22ecd8c564a231017d93ffc319adcbe9
Alex <125948626+allexyz@users.noreply.github.com>
2023-04-29 22:56:56 +0800

    Update main.js

--- Sodium-4/assets/js/main.js
+++ Sodium-4/assets/js/main.js
@@ -70,3 +70,9 @@
   }, true)
 
 })()
+
+async function addNavbar() {
+  const resp = await fetch("https://smc-26.github.io/sciencefair2023/navbar.html");
+  const html = await resp.text();
+  document.body.insertAdjacentHTML("afterbegin", html);
+}
